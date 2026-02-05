b4d57af362b2f83a0f0f9574ec07b759412ac4b9
Smash <test@gmail.com>
2026-02-04 20:20:56 -0500

    .

--- shift_register_segment_led/seven_segment.c
+++ shift_register_segment_led/seven_segment.c
@@ -1,6 +1,6 @@
 #include <wiringPi.h>
 #include <stdio.h>
-#define   SDI   0   //serial data input
+#define   SDI   3   //serial data input
 #define   RCLK  1   //memory clock input(STCP)
 #define   SRCLK 2   //shift register clock input(SHCP)
 unsigned char SegCode[16] = {0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f,0x77,0x7c,0x39,0x5e,0x79,0x71};
